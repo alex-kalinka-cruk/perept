6c6179519c4fb3047f19988ca0d47bcd9a385d61
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2019-12-31 15:12:10 +0100

    estPerf - to estimate performance when repeated tests are used.

---
+++ R/estPerf.R
@@ -0,0 +1,13 @@
+#' estPerf
+#'
+#' Estimate performance (false-positive rate, false-negative rate, etc) when repeated tests are used.
+#'
+#' @param n Integer specifying the number of tests that produced the expected result.
+#' @param result Integer value indicating whether the observed result was negative (0) or positive (1).
+#' @param expected Integer value indicating whether the expected result was negative (0) or positive (1).
+#'
+#' @return The estimated performance, a real value in the range [0,1].
+#' @export
+estPerf <- function(n, result, expected){
+
+}
