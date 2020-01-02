e83d4342d67f00fe35ddb6228d00442b4703afa9
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2020-01-02 12:06:23 +0100

    bpr_true fixed exponents issue.

---
+++ R/ODL.R
@@ -0,0 +1,5 @@
+#' ODL
+#'
+#' Calculate the Observed Data Likelihood (ODL) when true responses are unknown and repeated tests are performed on a set of test subjects (e.g. genes).
+#'
+#' @param
--- R/bpr_true.R
+++ R/bpr_true.R
@@ -1,10 +1,10 @@
 #' bpr_true
 #'
-#' Calculates a Bayesian posterior probability that a given test subject (e.g. a gene) has a given result (either negative or positive).
+#' Calculates a Bayesian posterior probability that a given test subject (e.g. a gene) has a given expected result (either negative or positive).
 #'
 #' @param n An integer specifying the number of tests that produced a particular result (either negative or positive) for the focal subject.
 #' @param N Integer value giving the total number of repeated tests for the given subject.
-#' @param expected An integer value (either 0 or 1) indicating whether the expected result was negative (0) or positive (1).
+#' @param expected An integer value (either 0 or 1) indicating whether the expected result is negative (0) or positive (1).
 #' @param theta_0 A real number (in range [0,1]) giving the prevalence of negative results for a set of test subjects.
 #' @param theta_1 A real number (in range [0,1]) giving the prevalence of positive results for a set of test subjects.
 #' @param p00 A real number (in range [0,1]) giving the probability that a test is negative given that the true result is negative (true-negative rate) for a set of test subjects.
@@ -17,13 +17,14 @@
 #' @export
 bpr_true <- function(n, N, expected, theta_0, theta_1, p00, p11, p01, p10){
   if(!expected %in% 0:1) stop("expected result can only be 0 (negative) or 1 (positive)")
+  if(N < n) stop("N must be greater than or equal to n")
   n_opp <- N-n
   tryCatch({
     if(expected == 0){
       num <- theta_0 * (p00^n) * (p10^n_opp)
-      denom <- num + (theta_1 * (p01^n) * (p11*n_opp))
+      denom <- num + (theta_1 * (p01^n) * (p11^n_opp))
     }else{
-      num <- theta_1 * (p01^n_opp) * (p11*n)
+      num <- theta_1 * (p01^n_opp) * (p11^n)
       denom <- num + (theta_0 * (p00^n_opp) * (p10^n))
     }
     res <- num/denom
