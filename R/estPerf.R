bb4bdcdc4c551aff24fecf499ff51533158d7ff1
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2020-01-02 10:30:18 +0100

    estPerf and estPrev first pass.

--- R/estPerf.R
+++ R/estPerf.R
@@ -1,13 +1,23 @@
 #' estPerf
 #'
-#' Estimate performance (false-positive rate, false-negative rate, etc) when repeated tests are used.
+#' Estimate performance (false-positive rate, false-negative rate, true-positive rate, true-negative rate) when there are repeated tests for a set of test subjects (e.g. genes).
 #'
-#' @param n Integer specifying the number of tests that produced the expected result.
-#' @param result Integer value indicating whether the observed result was negative (0) or positive (1).
-#' @param expected Integer value indicating whether the expected result was negative (0) or positive (1).
+#' @param n Vector of integers specifying the number of tests that produced a particular result (either negative or positive) for each subject.
+#' @param N Integer value giving the total number of repeated tests per subject.
+#' @param delta Vector of real numbers giving the probability that a given gene has the expected result (either negative or positive) - must be the same length as `n`.
 #'
 #' @return The estimated performance, a real value in the range [0,1].
+#' @examples \dontrun{estPerf(n,N,delta)}
 #' @export
-estPerf <- function(n, result, expected){
-
+estPerf <- function(n, N, delta){
+  if(length(n) != length(delta)) stop("n and delta must be the same length")
+  tryCatch({
+    n_opp <- N-n
+    num <- sum(n*delta)
+    denom <- num + sum(n_opp*delta)
+    res <- num/denom
+  },
+  error = function(e) stop(paste("'estPerf' error:",e))
+  )
+  return(res)
 }
---
+++ R/estPrev.R
@@ -0,0 +1,14 @@
+#' estPrev
+#'
+#' Estimate the prevalence of a particular result for a set of test subjects when there are repeated tests per subject (e.g. gene).
+#'
+#' @param delta Vector of real numbers giving the probability that a given gene has the expected result (either negative or positive).
+#'
+#' @result A real number giving the estimated prevalence of a given result (either negative or positive) for the set of subjects.
+#' @examples \dontrun{estPrev(delta)}
+#' @export
+estPrev <- function(delta){
+  tryCatch(estPrev <- sum(delta)/length(delta),
+           error = function(e) stop(paste("'estPrev' error:",e)))
+  return(estPrev)
+}
